38c9c69e21fa9d67377fa19eb7b953bac353efc3
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-01-28 21:26:24 +1000

    fixed python environment error

--- flake.nix
+++ flake.nix
@@ -1,21 +1,39 @@
 {
   description = "Zola flake";
   nixConfig.bash-prompt = "\[nix-develop\]$ ";
-  inputs.utils.url = "github:numtide/flake-utils";
-  outputs = { nixpkgs, utils, self, ... }:
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    utils = {
+      url = github:numtide/flake-utils;
+    };
+    # https://github.com/nix-community/poetry2nix/issues/734
+    # ModuleNotFoundError: No module named 'hatchling'
+    # Needed a more recent version of poetry2nix where overrides/build-systems.json had an entry for datadog Python package
+    # Can be removed when nixpkgs unstable has poetry2nix >=1.32.0
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+  };
+  };
+  outputs = { nixpkgs, utils, self, poetry2nix, ... }:
     utils.lib.eachDefaultSystem (system:
       let
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv;
         pkgs = import nixpkgs {
             inherit system;
           };
-          poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+          poetryEnv = mkPoetryEnv {
             projectDir = ./.;
             editablePackageSources = {
             };
           };
       in {
         devShell = pkgs.mkShell {
-          buildInputs = [ poetryEnv pkgs.zola ];
+          buildInputs = with pkgs; [ poetryEnv zola poetry ];
+            shellHook = ''
+            pre-commit install --install-hooks
+          '';
       };
     }
   );

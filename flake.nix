77e784c146332c378341f33c764d70a13f4e75b4
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2025-02-02 19:52:12 +1000

    wip: try switching out precommit

--- flake.nix
+++ flake.nix
@@ -6,33 +6,33 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
-    # Use p2nix directly for the updates
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+    arichtman = {
+      url = "github:arichtman/nix";
+    };
+    mado = {
+      url = "github:akiomik/mado";
     };
   };
-  outputs = { nixpkgs, flake-utils, self, poetry2nix, ... } :
+  outputs = { nixpkgs, flake-utils, ... }@inputs :
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
     (system:
       let
         pkgs = import nixpkgs {
             inherit system;
-            overlays = [poetry2nix.overlays.default];
         };
-        poetryEnv = pkgs.poetry2nix.mkPoetryEnv {
-            projectDir = ./.;
-          };
       in {
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
-              poetry
-              poetryEnv
+              inputs.mado.packages.${system}.default
               zola
               actionlint
+              inputs.arichtman.packages.${system}.prefligit
+              # Required for prefligit
+              uv
             ];
             shellHook = ''
-              pre-commit install --install-hooks
+              prefligit install
             '';
           };
     }
